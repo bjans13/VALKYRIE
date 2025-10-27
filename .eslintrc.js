7ed55c4a4452c50ba7c880c70321c0aaa8f3ba7e
bjans13 <167808030+bjans13@users.noreply.github.com>
2025-10-27 00:07:43 -0400

    Align lint rules with project style and add SSH handler tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
-  env: {
-    node: true,
-    es2021: true,
-    jest: true,
-  },
-  extends: ['eslint:recommended'],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-    'indent': ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
-    'no-unused-vars': ['warn'],
-    'no-console': ['warn'],
-  },
+    env: {
+        node: true,
+        es2021: true,
+        jest: true,
+    },
+    extends: ['eslint:recommended'],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {
+        indent: ['error', 4, { SwitchCase: 1, ignoreComments: true }],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+        'no-unused-vars': ['warn'],
+        'no-console': ['warn'],
+    },
 };
